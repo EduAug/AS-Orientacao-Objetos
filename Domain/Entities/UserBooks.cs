a0d1092865583f6ccd97419c653d0e27ef3bb2bf
EduAug <edu_03@outlook.com.br>
2023-06-25 13:20:27 -0300

    Usuario nao pode alugar mais de uma vez o mesmo livro

--- Domain/Entities/UserBooks.cs
+++ Domain/Entities/UserBooks.cs
@@ -11,7 +11,12 @@ namespace AS_Orientacao_Objetos.Domain.Entities
         //Quem tirou qual(is) livro(s) e quando.
         public int UserId { get; set; }
         public int BookId { get; set; }
-
+        //Por não existir "Id da transação" um usuário
+        //não poderá tornar a alugar um livro que já tenha
+        //alugado no passado.
+        //Caso isso venha a acontecer, será editada a
+        //relação e mudado o prazo limite e apagada a
+        //data de retorno.
         public User User { get; set; }
         public Book Book { get; set; }
 
